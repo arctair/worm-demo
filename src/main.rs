d5e63f47481182ed28112df2c85e9977d7e32c11
tyler <tyler@cruftbusters.com>
2024-01-08 14:33:45 -0800

    capsule ship on plane

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use std::f32::consts::PI;
+use bevy::a11y::AccessibilitySystem::Update;
 use bevy::app::{App, Startup};
 use bevy::asset::Assets;
 use bevy::DefaultPlugins;
-use bevy::math::Vec3;
-use bevy::prelude::{Camera2dBundle, Color, ColorMaterial, Commands, Component, Mesh, ResMut, shape, Transform};
-use bevy::sprite::MaterialMesh2dBundle;
+use bevy::math::{Quat, Vec3};
+use bevy::pbr::{CascadeShadowConfigBuilder, DirectionalLightBundle, PbrBundle, StandardMaterial};
+use bevy::prelude::{Camera3dBundle, Color, Commands, Component, DirectionalLight, Mesh, Query, ResMut, Transform};
+use bevy::prelude::shape::{Capsule, Plane};
+use bevy::transform::TransformBundle;
 use bevy::utils::default;
 
 fn main() {
@@ -14,25 +18,55 @@ fn main() {
 }
 
 #[derive(Component)]
-struct Tree;
+struct Ship;
 
 fn startup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-64., 32., -32.).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 
-    let p1 = 0.7548776662466927;
-    let p2 = 0.5698402909980532;
-    for index in 0..512 {
-        let x = (p1 * index as f32) % 1. * 512. - 256.;
-        let y = (p2 * index as f32) % 1. * 512. - 256.;
-        commands.spawn((Tree, MaterialMesh2dBundle {
-            mesh: meshes.add(shape::RegularPolygon::new(8., 8).into()).into(),
-            material: materials.add(ColorMaterial::from(Color::RED)),
-            transform: Transform::default().with_translation(Vec3::new(x, y, 0.)),
+    commands.spawn(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            shadows_enabled: true,
             ..default()
-        }));
-    }
+        },
+        transform: Transform {
+            translation: Vec3::new(0.0, 2.0, 0.0),
+            rotation: Quat::from_rotation_x(-PI / 4.),
+            ..default()
+        },
+        cascade_shadow_config: CascadeShadowConfigBuilder {
+            first_cascade_far_bound: 4.0,
+            maximum_distance: 10.0,
+            ..default()
+        }
+            .into(),
+        ..default()
+    });
+
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Plane::default().into()).into(),
+        material: materials.add(Color::WHITE.into()),
+        transform: Transform::default().with_scale(Vec3::splat(32.)),
+        ..default()
+    });
+
+    commands.spawn_empty()
+        .insert(
+            PbrBundle {
+                mesh: meshes.add(Capsule::default().into()).into(),
+                material: materials.add(Color::RED.into()),
+                transform: Transform::default(),
+                ..default()
+            }
+        )
+        .insert(TransformBundle::from(
+            Transform::from_xyz(0., 1., 0.)
+                .with_rotation(Quat::from_rotation_x(PI / 2.))
+        ));
 }
