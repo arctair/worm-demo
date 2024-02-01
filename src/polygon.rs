1fb1b9f3f95faf63c323070734e46d8c95ccd110
arctair <jtyler_@outlook.com>
2024-02-01 15:02:15 -0800

    sink is simple subtract

--- src/polygon.rs
+++ src/polygon.rs
@@ -1,5 +1,9 @@
 use bevy::math::Vec2;
 use bevy::prelude::Component;
+use bevy_rapier2d::na::Point2;
+use bevy_rapier2d::parry::shape::SegmentPointLocation;
+use bevy_rapier2d::parry::utils::segments_intersection2d;
+use bevy_rapier2d::parry::utils::SegmentsIntersection::{Point, Segment};
 use svg::node::element::Path;
 use svg::node::element::path::Data;
 
@@ -29,8 +33,105 @@ impl Polygon {
     }
 }
 
+#[derive(Clone, Copy, Debug)]
+enum TraceMode {
+    TracingSelf,
+    TracingBounds,
+}
+
 impl Polygon {
-    fn sink(self, area: f32, bounds: Polygon) -> Polygon { return self; }
+    fn sink(self, area: f32, bounds: Self) -> Self {
+        let mut new_vertices = vec![];
+        let mut trace_mode = TraceMode::TracingSelf;
+        let mut start_index = 0;
+        let mut end_index = 1;
+        let mut start_bounds_index = 1;
+        let mut end_bounds_index = 0;
+
+        while new_vertices.is_empty() || start_index != 0 || match trace_mode {
+            TraceMode::TracingBounds => true,
+            _ => false
+        } {
+            let mut intersection = None;
+            match trace_mode {
+                TraceMode::TracingSelf => {
+                    new_vertices.push(self.vertices[start_index]);
+                    for _ in 0..bounds.vertices.len() {
+                        let start = self.vertices[start_index];
+                        let end = self.vertices[end_index];
+                        let start_bounds = bounds.vertices[start_bounds_index];
+                        intersection = segments_intersection2d(
+                            &Point2::from(start),
+                            &Point2::from(end),
+                            &Point2::from(start_bounds),
+                            &Point2::from(bounds.vertices[end_bounds_index]),
+                            0.01,
+                        ).and_then(|intersection| {
+                            if (end.x - start.x) * (start_bounds.y - start.y) - (end.y - start.y) * (start_bounds.x - start.x) > 0. {
+                                Some(intersection)
+                            } else {
+                                None
+                            }
+                        });
+                        if intersection.is_some() { break; }
+
+                        start_bounds_index = end_bounds_index;
+                        end_bounds_index = (end_bounds_index + bounds.vertices.len() - 1) % bounds.vertices.len();
+                    }
+                }
+                TraceMode::TracingBounds => {
+                    new_vertices.push(bounds.vertices[start_bounds_index]);
+                    for _ in 0..self.vertices.len() {
+                        intersection = segments_intersection2d(
+                            &Point2::from(self.vertices[start_index]),
+                            &Point2::from(self.vertices[end_index]),
+                            &Point2::from(bounds.vertices[start_bounds_index]),
+                            &Point2::from(bounds.vertices[end_bounds_index]),
+                            0.01,
+                        );
+                        if intersection.is_some() { break; }
+
+                        start_index = end_index;
+                        end_index = (end_index + 1) % self.vertices.len();
+                    }
+                }
+            }
+
+
+            match intersection {
+                Some(Point { loc1: location, .. }) => {
+                    match location {
+                        SegmentPointLocation::OnVertex(_) => {}
+                        SegmentPointLocation::OnEdge([_, from_start]) => {
+                            let start = self.vertices[start_index];
+                            let end = self.vertices[end_index];
+                            new_vertices.push(start + from_start * (end - start));
+
+                            match trace_mode {
+                                TraceMode::TracingSelf => trace_mode = TraceMode::TracingBounds,
+                                TraceMode::TracingBounds => trace_mode = TraceMode::TracingSelf,
+                            }
+                        }
+                    }
+                }
+                Some(Segment { .. }) => {}
+                None => {}
+            }
+
+            match trace_mode {
+                TraceMode::TracingSelf => {
+                    start_index = end_index;
+                    end_index = (end_index + 1) % self.vertices.len();
+                }
+                TraceMode::TracingBounds => {
+                    start_bounds_index = end_bounds_index;
+                    end_bounds_index = (end_bounds_index + bounds.vertices.len() - 1) % bounds.vertices.len();
+                }
+            }
+        }
+
+        return Self::from(new_vertices);
+    }
 }
 
 impl From<Vec<Vec2>> for Polygon {
@@ -56,7 +157,7 @@ mod tests {
     }
 
     #[test]
-    fn test_sink() {
+    fn test_sink_simple_subtract() {
         let left_operand = Polygon::from(vec![
             Vec2::new(2., 2.),
             Vec2::new(2., -2.),
@@ -91,6 +192,6 @@ mod tests {
             .add(right_operand.svg_path("white", 0.125 / 4.))
             ;
 
-        assert_eq!(actual, expected, "Visual: {:?}", save_svg(scene, "test_sink"))
+        assert_eq!(actual, expected, "Visual: {:?}", save_svg(scene, "test_sink_simple_subtract"))
     }
 }
